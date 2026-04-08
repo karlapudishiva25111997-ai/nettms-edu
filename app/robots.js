ca91660047e0c6c4cce44eb7f13718dfbfa64251
karlapudishiva25111997-ai <karlapudishiva25111997@gmail.com>
2026-04-08 22:02:09 +0530

    Fixed robots.txt to prevent image indexing

--- app/robots.js
+++ app/robots.js
@@ -1,8 +1,17 @@
- export default function robots() {
+export default function robots() {
   return {
     rules: {
       userAgent: '*',
       allow: '/',
+      disallow: [
+        '/logo.jpg',
+        '/favicon.ico',
+        '/iso-logo.png',
+        '/startup-india.jpg',
+        '/*.jpg$',
+        '/*.png$',
+        '/*.ico$',
+      ],
     },
     sitemap: 'https://nettmsedu.in/sitemap.xml',
   }
